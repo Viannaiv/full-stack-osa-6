0993e378c4fe4a25f12de22035854b95842c22bc
Vivianna Tilander <viva95@windowslive.com>
2020-03-01 05:50:25 +0200

    Tehtävä 6.2: unicafe revisited, step2

--- unicafe-redux/src/index.js
+++ unicafe-redux/src/index.js
@@ -7,20 +7,32 @@ const store = createStore(reducer)
 
 const App = () => {
   const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
+    store.dispatch({ type: 'GOOD' })
+  }
+
+  const bad = () => {
+    store.dispatch({ type: 'BAD' })
+  }
+
+  const ok = () => {
+    store.dispatch({ type: 'OK' })
+  }
+
+  const zero = () => {
+    store.dispatch({ type: 'ZERO' })
   }
 
   return (
     <div>
-      <button onClick={good}>hyvä</button> 
-      <button>neutraali</button> 
-      <button>huono</button>
-      <button>nollaa tilastot</button>
-      <div>hyvä {store.getState().good}</div>
-      <div>neutraali</div>
-      <div>huono</div>
+      <button onClick={good}>Hyvä</button> 
+      <button onClick={ok}>Neutraali</button> 
+      <button onClick={bad}>Huono</button>
+      <button onClick={zero}>Nollaa tilastot</button>
+      
+      <h2>Tilastot</h2>
+      <div>Hyvä: {store.getState().good}</div>
+      <div>Neutraali: {store.getState().ok}</div>
+      <div>Huono: {store.getState().bad}</div>
     </div>
   )
 }
