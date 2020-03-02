618d07c40266f83346904bacec153fbc3d604738
Vivianna Tilander <viva95@windowslive.com>
2020-03-02 07:00:04 +0200

    Tehtävä 6.13: anekdootit ja backend, step1

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { useDispatch } from 'react-redux'
+import { initialiseAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
+
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => 
+      dispatch(initialiseAnecdotes(anecdotes))
+    )
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
